2be0e613df39ec446f9e63a3889ca788be912192
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-02-08 02:30:23 +0900

    feat: update deps to use @mdx-js/mdx@2.0.0

--- deps.ts
+++ deps.ts
@@ -5,5 +5,8 @@ export type {
   Plugin,
   ResolveResult,
 } from "https://deno.land/x/aleph@v0.3.0-beta.19/types.d.ts";
-export { compile } from "https://esm.sh/xdm@1.12.2?deps=unist-util-stringify-position@3.0.0,unist-util-visit@3.0.0";
+export {
+  compile,
+  type CompileOptions,
+} from "https://esm.sh/@mdx-js/mdx@2.0.0?pin=v66";
 export { default as util } from "https://deno.land/x/aleph@v0.3.0-beta.19/shared/util.ts";
